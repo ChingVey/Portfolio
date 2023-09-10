1da9a28adc5e3d18984b52665f23b4c3c6791f9f
Ayzen126 <134722522+Ayzen126@users.noreply.github.com>
2023-09-10 22:35:02 +0700

    5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -73,7 +73,7 @@ onClickOutside(modal, () => (ModalOpen.value = false))
 
       <div class="inlineblock pt-12">
         <div class="flex justify-center items-center">
-          <img class="w-80 sm:w-80 md:w-96 object-cover bg-transparent rounded-[200px]" src="/TranPhoto.png">
+          <img class="w-72 sm:w-80 md:w-96 object-cover bg-transparent rounded-[200px]" src="/TranPhoto.png">
         </div>
         <h1 class="text-5xl sm:text-4xl md:text-5xl font-bold text-center pt-4"> Lor ChingVey</h1>
         <p class="font-bold text-xl pt-2 text-center">- Introduction</p>
